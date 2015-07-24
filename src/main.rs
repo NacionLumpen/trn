b5e26ba95a1cbc92981113bcbb47dcb226c1a849
Sebastian Ortega <sortega@coinffeine.com>
2015-07-24 20:02:41 +0200

    Make the point fall in a toroidal space

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,21 @@ fn main() {
         .exit_on_esc(true)
         .into();
 
+    let mut pos = [win_size[0] as f64 / 2.0, win_size[1] as f64 / 2.0];
+    
     for frame in window {
         frame.draw_2d(|c, g| {
             clear([0.0, 0.0, 0.0, 1.0], g);
             rectangle(
                 [1.0; 4],
-                [win_size[0] as f64 / 2.0, win_size[1] as f64 / 2.0, 1.0, 1.0],
+                [pos[0], pos[1], 1.0, 1.0],
                 c.transform,
                 g);
-        })
+        });
+
+        pos[1] += 1.0;
+        if pos[1] as u32 > win_size[1] {
+            pos[1] -= win_size[1] as f64;
+        }
     }
 }
