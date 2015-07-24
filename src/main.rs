dd06eb2c61f0203b87e1db18f725de7be59dee9e
Sebastian Ortega <sortega@coinffeine.com>
2015-07-24 19:27:52 +0200

    Show a blank window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate piston_window;
+
+use piston_window::*;
+
+#[allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    let window: PistonWindow = WindowSettings::new("TRN", [640, 480])
+        .exit_on_esc(true)
+        .into();
+    for _ in window {}
 }
