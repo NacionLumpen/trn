7315ed29b19e98d51fa2f928f5795911557e9a7a
Sebastian Ortega <sortega@coinffeine.com>
2015-07-24 19:53:32 +0200

    Display a fixed point in the middle of the screen

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,20 @@ use piston_window::*;
 
 #[allow(dead_code)]
 fn main() {
-    let window: PistonWindow = WindowSettings::new("TRN", [640, 480])
+    let win_size = [640, 480];
+    
+    let window: PistonWindow = WindowSettings::new("TRN", win_size)
         .exit_on_esc(true)
         .into();
-    for _ in window {}
+
+    for frame in window {
+        frame.draw_2d(|c, g| {
+            clear([0.0, 0.0, 0.0, 1.0], g);
+            rectangle(
+                [1.0; 4],
+                [win_size[0] as f64 / 2.0, win_size[1] as f64 / 2.0, 1.0, 1.0],
+                c.transform,
+                g);
+        })
+    }
 }
